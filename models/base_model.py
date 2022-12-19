bbf1d74b132f879d7aa8fd42688a54c239e85777
andpuc23 <44880821+andpuc23@users.noreply.github.com>
2022-12-19 17:48:04 +0300

    added full data, its preprocessing, and model's interface

---
+++ models/base_model.py
@@ -0,0 +1,9 @@
+class Base_model:
+    def __init__(self):
+        pass
+    
+    def fit(self, data):
+        pass
+    
+    def predict(self, data):
+        pass
--- utils/prepare_data.py
+++ utils/prepare_data.py
@@ -11,3 +11,36 @@ def prepare_short_data(filename:str='data/returns_RU.xlsx') -> pd.DataFrame:
         df[col+'_pct'] = df[col].pct_change()
     
     return df
+
+def prepare_full_data(filename:str='data/Data_RU.xlsx') -> pd.DataFrame:
+    """
+    reads data from excel file, fills weekday indices, percent changes for stocks
+    returns filled dataframe
+    """
+    df = pd.read_excel(filename, skiprows=1, parse_dates=[0])
+    df.drop([0], inplace=True)
+    df['Date'] = pd.to_datetime(df['SECID'], format="%Y-%m-%d %H:%M:%S")
+    df.drop(columns=['SECID'])
+    
+    for col in df.columns[1:]:
+        if col[-2] == '.':
+            if col[-1] == '1':
+                df.rename(columns={col:col[:-2]+'_high'}, inplace=True)
+            elif col[-1] == '2':
+                df.rename(columns={col:col[:-2]+'_low'}, inplace=True)
+            elif col[-1] == '3':
+                df.rename(columns={col:col[:-2]+'_open'}, inplace=True)
+            else:
+                df.rename(columns={col:col[:-2]+'_volume'}, inplace=True)
+        else:
+            df.rename(columns={col:col+'_close'}, inplace=True)
+        
+    for col in df.columns[1:100]:
+        col_name = col[:-6]
+        df[col_name+'_D1'] = (df[col_name+'_open']-df[col_name+'_close'].shift(periods=-1))/df[col_name+'_close'].shift(periods=-1)
+        df[col_name+'_D2'] = (df[col_name+'_close']-df[col_name+'_open'])/df[col_name+'_open']
+        df[col_name+'_D3'] = (df[col_name+'_high']-df[col_name+'_open'])/df[col_name+'_open']
+        df[col_name+'_D4'] = (df[col_name+'_open']-df[col_name+'_low'])/df[col_name+'_low']
+        
+    return df
+        
